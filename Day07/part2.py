bf84689daac1fb130160af8acacc384fc3f786c7
SteveKaserNKP <steve.kaser@nkparts.com>
2021-12-08 17:40:22 -0500

    solved Day 7 Part 2

--- Day07/part2.py
+++ Day07/part2.py
@@ -14,7 +14,10 @@ def parse_input(file_path: str) -> Any:
 def calc_fuel_usage(crabs: List[int], target: int) -> int:
     fuel = 0
     for crab in crabs:
-        fuel += abs(crab-target)
+        moves_needed = abs(crab-target)
+        while moves_needed > 0:
+            fuel += moves_needed
+            moves_needed -= 1
     return fuel
 
 if __name__ == '__main__':
@@ -25,8 +28,10 @@ if __name__ == '__main__':
         input_file = os.path.join(day_dir, 'test_input.txt')
 
     crabs = parse_input(input_file)
+    crabs.sort()
     max_x = max(crabs)
     possible_x = []
     for x in range(max_x):
+        print(x)
         possible_x.append(calc_fuel_usage(crabs,x))
     print(min(possible_x))
